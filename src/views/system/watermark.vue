0eb2b0be3a0dd0f8dff0e5f6923e2adb8fa50eee
xd <xd@xddeiMac.local>
2017-11-21 14:26:02 +0800

    第一次提交

---
+++ src/views/system/watermark.vue
@@ -0,0 +1,16 @@
+<template>
+   <div>
+   	 <header-nav></header-nav>
+   	 <router-view></router-view>	
+   </div>
+</template>
+
+<script>
+  import Header from '@/components/header'
+
+  export default {
+  	components:{
+  		headerNav:header
+  	}
+  }
+</script>
